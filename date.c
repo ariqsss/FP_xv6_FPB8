66bacdfd435cdf7af72b1ad5118e2acd3e0d2d39
Unknown <akbar.exp57@gmail.com>
2017-12-03 06:31:01 +0700

    need to change name

--- date.c
+++ date.c
@@ -18,8 +18,19 @@ printf(1,"called at %d:%d:%d\n",r.hour,r.minute,r.second);
 if (argc==2){
 struct rtcdate now;
 
-	int coba=0;
-	for (int i=0; i < strlen(argv[1]) ; ++i){
+int decrement=0;
+char b=argv[1][strlen(argv[1])-1];
+
+if (b=='s') ++decrement;
+else if (b=='m') ++decrement;
+else if (b=='h') ++decrement;
+else if (b=='d') ++decrement;
+
+//printf(1,"%c\n",b);
+
+	long long coba=0;
+	for (int i=0; i < strlen(argv[1])-decrement ; ++i){
+//	  printf(1,"%c\n",argv[1][i]);
 	  int timer=10;
 	  if (argv[1][i]=='1'){ coba+=1;}
 	  else if (argv[1][i]=='2') {coba+=2;}
@@ -32,24 +43,29 @@ struct rtcdate now;
 	  else if (argv[1][i]=='9') {coba+=9;}
 	  else  coba+=0;
 
-	  if (i+1 < strlen(argv[1])) coba*=timer;
+	  if (i+1 < strlen(argv[1])-decrement) coba*=timer;
 	}
 
-int wadah=0; //wadah untuk selisih menit
-int temp; //wadah untuk jarak selisih detik yg dituju dgn sekarang
+if (decrement!=0){
+ if (b=='m') coba=coba*60; //convert minute to second
+ else if (b=='h') coba=coba*3600; //convert hour to second
+ else if (b=='d') coba=coba*3600*24; //convert day to second
+}
+
+//printf(1,"%d",coba);
+long long temp_time=0;
+temp_time+=r.second;
+int has_been_gone_x_second=0;
 if (date(&now)){ printf(2,"date failed\n"); exit();}
-while (1){
 
-  
- 	temp=now.second-r.second; // cek selisih detik
-	wadah=now.minute-r.minute; // lalu dijumlah dengan selisih menit * 60
-	temp=temp+wadah*60;
-if(temp == coba){ break;} // jika sudah = coba break
-
+while (has_been_gone_x_second != coba){
+	if (now.second!=temp_time){ //when the second is changing
+	has_been_gone_x_second++; //increment clock
+	temp_time=now.second; //change the temp to now to check the 'next' now
+	}
  if (date(&now)){
   exit();
  }
-
 }
 printf(1,"printed at %d:%d:%d\n",now.hour,now.minute,now.second);
 
