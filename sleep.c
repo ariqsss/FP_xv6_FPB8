149ca5d19302e5cf5363a9804c2dd81638cabb48
Unknown <akbar.exp57@gmail.com>
2017-12-04 23:37:19 +0700

    ???? sleep?

---
+++ sleep.c
@@ -0,0 +1,14 @@
+#include "types.h"
+#include "user.h"
+
+int main(int argc, char *argv[]){
+//int sleep_sec;
+if (argc==2){
+	int sleep_sec;
+	sleep_sec = atoi(argv[1]);
+	sleep(sleep_sec);
+
+printf(1,"%d Succeed\n",sleep_sec);
+}
+exit();
+}
